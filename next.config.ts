82761b1620b402c5c68c05a3ca70b2ed88e47148
jyooni <berry3891@naver.com>
2026-01-11 20:02:03 +0900

    chore: 불필요한 @next/mdx 관련 라이브러리 제거 - next-mdx-remote로 동적 렌더링 하므로 제거해도 O

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["mdx", "ts", "tsx"],
+  pageExtensions: ["ts", "tsx"],
   reactStrictMode: true,
 };
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
